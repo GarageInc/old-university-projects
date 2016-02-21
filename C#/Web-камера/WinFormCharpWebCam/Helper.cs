029804ce902fdd0de452c8217486f45d05b250b2
GarageInc <9-b-rinat@rambler.ru>
2016-02-21 22:50:08 +0300

    up

---
+++ C#/Web-камера/WinFormCharpWebCam/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormCharpWebCam
+{
+    class Helper
+    {        
+        //Для сохранения ряда картинок
+        public static void SaveImageCapture(System.Drawing.Image image, int i=0)
+        {
+            try
+            {
+                if (image != null)
+                {
+                    image.Save("D:\\Image"+i+".jpg");
+                    
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Были проблемы с сохранением файла." +
+                    "Проверьте права доступа.");
+            }
+        }
+    }
+}
