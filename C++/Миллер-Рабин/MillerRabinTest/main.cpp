8ff2f99866862ed6ca83a489affe4080d9c59322
GarageInc <9-b-rinat@rambler.ru>
2016-02-28 15:18:35 +0300

    up6

--- C++/Миллер-Рабин/MillerRabinTest/main.cpp
+++ C++/Миллер-Рабин/MillerRabinTest/main.cpp
@@ -1,25 +1,42 @@
+
+//#pragma comment (lib,"mpfr.lib")
+//#pragma comment (lib,"mpir.lib")
+
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
 #include <conio.h>
 #include <string>
-
 #include <time.h>
-
 #include <thread>
 #include <chrono>
 #include <future>
 #include <mutex>
 
 
+#include<boost\multiprecision\cpp_int.hpp>
+#include<boost\any.hpp>
+using namespace boost::multiprecision;
+
+//#include "mpreal.h"
 #include "functions.h"
 #include "miller-rabin.h"
 #include"primegen.h"
 #include"example1.h"
 #include"example2.h"
-#include"BigInt.h"
+
+// Максимальное uint1024_t: 2*179769313486231590772930519078902473361797697894230657273430081157732
+//675805500963132708477322407536021120113879871393357658789768814416622492
+//847430639474124377767893424865485276302219601246094119453082952085005768
+//838150682342462881473913110540827237163350510684586298239947245938479716
+//304835356329624224137216
+
+// Максимальное uint128_t: 2*340282366920938463463374607431768211456
+
+// Максимальное uint64_t: 2*18446744073709551616
 
 using namespace std;
+
 
 
 // Используемое количество потоков. Будет равно числу ядер в компьютере
@@ -31,26 +48,14 @@ const int MAX_THREADS_COUNT = 100;
 // Потоки
 thread THREADS[MAX_THREADS_COUNT];
 
-// Функция ожидания окончания работы потоков
-void waitEnding(atomic<bool> *COMPLETED_THREADS) {
-
-	printf("\n\nОжидание завершения ещё работающих потоков");
-	for (int j = 0; j < THREADS_COUNT; j++) {
-			
-		if (THREADS[j].joinable()) {
-
-			THREADS[j].join();
-		}
-
-		printf("\nЗавершен поток %d", j);
-	}
-}
-
 
 int main() {
 	
 	setlocale(LC_ALL, "Russian");// Чтобы выводился текст на русском языке
-		 
+	// const int digits = 50;// 4096;
+	// mpreal::set_default_prec(mpfr::digits2bits(digits));
+	// mpf_set_default_prec(4096);
+
 	// Количество ядер
 	int NUM_CORES = thread::hardware_concurrency();
 	// Количество потоков
@@ -63,6 +68,7 @@ int main() {
 	
 	// Массив булевых значений - для контроля работы потоков(освобождения и заполнения)
 	atomic<bool> COMPLETED_THREADS[MAX_THREADS_COUNT];
+
 	// Говорим, что потоки ещё свободны
 	for (int j = 0; j < THREADS_COUNT; j++) {
 		COMPLETED_THREADS[j] = true;
@@ -73,13 +79,10 @@ int main() {
 	// Начало работы
 	clock_t start_at = clock();
 
-	run1(FOUT_FILES,COMPLETED_THREADS, THREADS_COUNT, THREADS);
 	// Запускается функция, которая проверяет псевдопростые числа: числа, которые являются произведением двух простых чисел 'p' и 'q', но проходят тест Миллера-Рабина по 	базе 'A'
-	//run2(FOUT_FILES, COMPLETED_THREADS, THREADS, THREADS_COUNT);
-
-	// Ждем окончания работы всех потоков
-	waitEnding(COMPLETED_THREADS);
-
+	//run1(FOUT_FILES,COMPLETED_THREADS, THREADS_COUNT, THREADS);
+	run2(FOUT_FILES, COMPLETED_THREADS, THREADS, THREADS_COUNT);
+	
 	// Конец работы
 	clock_t finish_at = clock();
 
